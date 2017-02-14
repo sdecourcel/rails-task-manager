ec3b70496fce104444edb5dfd47e048aec854509
Sibylle de Courcel <sibylle44@gmail.com>
2017-02-14 12:44:44 +0100

    index show nearly all done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  root 'tasks#index'
+
+  get 'tasks' => 'tasks#index'
+  get 'tasks/:id' => 'tasks#show', as: :task
+
+  get 'tasks/new' => 'tasks#new'
+  post 'tasks'  => 'tasks#create'
+
+  get 'tasks/:id/edit' => 'tasks#edit'
+  patch 'tasks/:id' => 'tasks#update'
+
+  delete 'tasks/:id' =>  'tasks#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
